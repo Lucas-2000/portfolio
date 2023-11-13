023d3e576645edc9163cc76abe6c288356fedabd
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-11-13 11:35:57 -0300

    [feat] implemented experiences page

---
+++ src/app/experiencias/experiences-list/experiencesList.ts
@@ -0,0 +1,43 @@
+export const experiencesList = [
+  {
+    id: 1,
+    title: "Suporte técnico Help Desk Júnior",
+    company: "DPATEC - Equipamentos de ponto",
+    localization: "Mauá - SP",
+    skills: ["Redes", "Infraestrutura", "SQL", "Access"],
+    summary:
+      "Estagiário em suporte técnico no sistema de ponto Sapewin, resolução de problemas dos usuários, erros no sistemas, dúvidas na utilização, implantação/atualização do sistema e treinamento do sistema para os clientes.",
+    startDate: "22/10/2019",
+    finalDate: "10/02/2022",
+  },
+  {
+    id: 2,
+    title: "Desenvolvedor Web Júnior",
+    company: "New Century",
+    localization: "Santo André - SP",
+    skills: ["ASP.Net", "C#", "Oracle", "Firebird", "SQL"],
+    summary:
+      "Desenvolvedor ASP.Net em um sistema transformando as funcionalidades do sistema desktop Delphi em web. Contato com bancos de dados Oracle, Firebird e SQL.",
+    startDate: "14/02/2022",
+    finalDate: "31/03/2022",
+  },
+  {
+    id: 3,
+    title: "Estagiário de TI - Analista de negócios",
+    company: "Amcor",
+    localization: "Mauá - SP",
+    skills: [
+      "Service Now",
+      "SQL",
+      "Excel",
+      "Word",
+      "Power Point",
+      "Scrum",
+      "Kanban",
+    ],
+    summary:
+      "Atuando no time de sistemas como analista de negócios resolvendo problemas do cliente interno, gerenciando chamados via Service Now, atuando em demandas e projetos, criação de documentações, contato com SQL e análise de código para possíveis melhorias e resoluções de problemas.",
+    startDate: "18/05/2022",
+    finalDate: "Atual",
+  },
+];
---
+++ src/app/experiencias/page.tsx
@@ -0,0 +1,51 @@
+import Footer from "../components/footer/footer";
+import Header from "../components/header/header";
+import { experiencesList } from "./experiences-list/experiencesList";
+
+export default function Experiencias() {
+  return (
+    <div>
+      <main className="max-w-7xl mx-auto px-4 sm:px-8 py-4 sm:py-8">
+        <div>
+          <Header />
+          <hr />
+        </div>
+        <section className="mt-4 sm:mt-8">
+          <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold">
+            Minhas experiências
+          </h1>
+        </section>
+        <section className="grid grid-cols-1 sm:grid-cols-3 gap-8 mt-16 h-auto sm:h-56 bg-dark text-white">
+          {experiencesList.map((experiences) => (
+            <div
+              key={experiences.id}
+              className="p-4 border border-gray-700 rounded-md"
+            >
+              <h2 className="text-xl font-bold mb-2 text-white">
+                {experiences.title}
+              </h2>
+              <p className="mb-2">
+                {experiences.company} - {experiences.localization}
+              </p>
+              <p className="mb-2">
+                {experiences.startDate} - {experiences.finalDate}
+              </p>
+              <p className="mb-2 text-justify">{experiences.summary}</p>
+              <ul className="flex flex-wrap">
+                {experiences.skills.map((skill) => (
+                  <li
+                    key={skill}
+                    className="mb-2 mr-2 px-3 py-1 bg-gray-700 text-white rounded-full"
+                  >
+                    {skill}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+}
